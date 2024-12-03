70f35434eb8ae57d14d32a0f6bc9af0855ad17bf
TomasPayeras <tomaspayeras@paucasesnovescifp.cat>
2024-12-03 17:02:24 +0100

    classe dia 03/12/24

---
+++ Tema 3/teoria.js
@@ -0,0 +1,10 @@
+
+function suma(param1,param2){
+    return param1 + param2;
+}
+
+let suma = (param1,param2) => {
+    return param1 + param2; // Retorno explícita
+}
+
+let suma = (param1,param2) => param1 + param2; // Retorno implícito
